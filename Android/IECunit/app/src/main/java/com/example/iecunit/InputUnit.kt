5094007c94442684e82a12a3c0e3d6d687056424
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2025-01-08 09:58:09 +0700

    ..

--- Android/IECunit/app/src/main/java/com/example/iecunit/InputUnit.kt
+++ Android/IECunit/app/src/main/java/com/example/iecunit/InputUnit.kt
@@ -1,27 +1,20 @@
 package com.example.iecunit
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Welcome : AppCompatActivity() {
+class InputUnit : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_welcome)
+        setContentView(R.layout.activity_input_unit)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
- val Btn=findViewById<ImageButton>(R.id.imageButton)
-     Btn.setOnClickListener {
-  val intent= Intent(this,MainActivity::class.java)
-  startActivity(intent)
-     }
     }
 }
