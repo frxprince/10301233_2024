59baa0201e20f464b24cb5db157b45d81d72710e
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2025-01-08 10:33:04 +0700

    ..

--- Android/IECunit/app/src/main/java/com/example/iecunit/MainActivity.kt
+++ Android/IECunit/app/src/main/java/com/example/iecunit/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.iecunit
 
 import android.content.Intent
+import android.icu.text.DecimalFormat
 import android.os.Bundle
 import android.renderscript.ScriptGroup.Input
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,10 +27,21 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     val BtnInputUnit=findViewById<Button>(R.id.button)
+    val txtInput=findViewById<EditText>(R.id.editTextNumberDecimal)
+    val txtview=findViewById<TextView>(R.id.textView)
+    var inBytes=0.0
  val InputUnitR=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
      result:ActivityResult->
      if(result.resultCode== RESULT_OK){
-       Log.v("Result",result.data?.getStringExtra("data")?:"No data")
+      inBytes= when(result.data?.getStringExtra("data")?:"No data"){
+        "kB"->txtInput.text.toString().toDouble()* Math.pow(10.0,3.0)
+        "MB"->txtInput.text.toString().toDouble()* Math.pow(10.0,6.0)
+        "GB"->txtInput.text.toString().toDouble()* Math.pow(10.0,9.0)
+        "TB"->txtInput.text.toString().toDouble()* Math.pow(10.0,12.0)
+        else-> 0.0
+      }
+      txtview.text=DecimalFormat("#,###,###").format(inBytes)+" bytes"
+      // Log.v("Result",result.data?.getStringExtra("data")?:"No data")
      }
  }
 
