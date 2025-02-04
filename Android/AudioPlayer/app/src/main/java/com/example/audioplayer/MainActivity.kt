72e355b7c86e859693b8ab4af2a11a55964d89e9
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2025-02-04 14:51:19 +0700

    ..

--- Android/AudioPlayer/app/src/main/java/com/example/audioplayer/MainActivity.kt
+++ Android/AudioPlayer/app/src/main/java/com/example/audioplayer/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.audioplayer
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -17,12 +23,35 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-val btnRes=findViewById<Button>(R.id.button)
+        val btnRes=findViewById<Button>(R.id.button)
         val btnAsset=findViewById<Button>(R.id.button2)
         val btnSDCARD=findViewById<Button>(R.id.button3)
         val btnInternet=findViewById<Button>(R.id.button4)
         val btnPlay=findViewById<Button>(R.id.button5)
         val btnPause=findViewById<Button>(R.id.button6)
         val btnStop=findViewById<Button>(R.id.button7)
+    lateinit var Mp:MediaPlayer
+if(ActivityCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)
+    != PackageManager.PERMISSION_GRANTED){
+ ActivityCompat.requestPermissions(this,
+     arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),12321)
+}
+btnSDCARD.setOnClickListener {
+Mp=MediaPlayer()
+    Log.v("folder",Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).path)
+Mp.setDataSource(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).path
++"/song.mp3")
+Mp.prepare()
+}
+        btnAsset.setOnClickListener {
+    var audiofile=assets.openFd("audio/tada.mp3")
+            Mp= MediaPlayer()
+    Mp.setDataSource(audiofile.fileDescriptor,audiofile.startOffset,audiofile.length)
+    Mp.prepare()
+        }
+        btnRes.setOnClickListener {
+        Mp= MediaPlayer() ; Mp= MediaPlayer.create(this,R.raw.greeting)
+        }
+        btnPlay.setOnClickListener { Mp.start() }
     }
 }
