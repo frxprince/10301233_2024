a255dd16a1a58875b356b074586d7df0e41979f1
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 <frxprince@gmail.com>
2025-02-04 16:22:02 +0700

    ...

--- Android/videoplayer/app/src/main/java/com/example/videoplayer/MainActivity.kt
+++ Android/videoplayer/app/src/main/java/com/example/videoplayer/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.videoplayer
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.VideoView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -26,7 +29,12 @@ class MainActivity : AppCompatActivity() {
     val btnPause=findViewById<Button>(R.id.button5)
     val btnStop=findViewById<Button>(R.id.button6)
     val wVideoView=findViewById<VideoView>(R.id.videoView)
-
+if(ActivityCompat.checkSelfPermission(this,Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED ){
+ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.INTERNET),12124)
+}
+   btnInternet.setOnClickListener {
+       wVideoView.setVideoURI(Uri.parse("http://www.drpaween.com/ohm/cs436/mv.mp4"))
+   }
     btnRes.setOnClickListener {
 wVideoView.setVideoURI(Uri.parse("android.resource://"+ packageName+"/"+R.raw.video2 ))
     }
