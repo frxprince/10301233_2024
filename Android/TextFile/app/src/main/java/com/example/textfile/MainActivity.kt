6df87574d4469bf9fbd8b942df941252e52a405f
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 <frxprince@gmail.com>
2025-02-05 11:10:19 +0700

    ..

--- Android/TextFile/app/src/main/java/com/example/textfile/MainActivity.kt
+++ Android/TextFile/app/src/main/java/com/example/textfile/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.textfile
 
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +27,28 @@ class MainActivity : AppCompatActivity() {
    val btnReadSD=findViewById<Button>(R.id.button4)
    val txtFname=findViewById<EditText>(R.id.editTextText)
    val txtData=findViewById<EditText>(R.id.editTextTextMultiLine)
+
+
+btnReadSD.setOnClickListener {
+    val file= File(Environment.getExternalStorageDirectory().absolutePath+"/"+
+            Environment.DIRECTORY_DOCUMENTS+"/"+txtFname.text.toString())
+    val lines=file.bufferedReader(Charsets.UTF_8).readLines()
+    var msg=""
+    for(line in lines)
+        msg=msg+line+"\n"
+    txtData.setText(msg)
+}
+
+btnWriteSD.setOnClickListener {
+    val file= File(Environment.getExternalStorageDirectory().absolutePath+"/"+
+    Environment.DIRECTORY_DOCUMENTS+"/"+txtFname.text.toString())
+    val outputfile=FileOutputStream(file)
+    outputfile.write(txtData.text.toString().toByteArray())
+    outputfile.flush()
+    outputfile.close()
+}
+
+
 btnAssets.setOnClickListener {
     val file=assets.open("textfile/pangram.txt")
     val lines=file.bufferedReader(Charsets.UTF_8).readLines()
