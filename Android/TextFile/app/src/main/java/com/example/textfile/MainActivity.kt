5499e3c946075d6bc2a51f04d323c51a9e329c4b
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 <frxprince@gmail.com>
2025-02-05 10:55:23 +0700

    ..

--- Android/TextFile/app/src/main/java/com/example/textfile/MainActivity.kt
+++ Android/TextFile/app/src/main/java/com/example/textfile/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.textfile
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+   val btnResource=findViewById<Button>(R.id.button)
+   val btnAssets=findViewById<Button>(R.id.button2)
+   val btnWriteSD=findViewById<Button>(R.id.button3)
+   val btnReadSD=findViewById<Button>(R.id.button4)
+   val txtFname=findViewById<EditText>(R.id.editTextText)
+   val txtData=findViewById<EditText>(R.id.editTextTextMultiLine)
+
+  btnResource.setOnClickListener {
+      val file=resources.openRawResource(R.raw.genesis01)
+      val lines=file.bufferedReader(Charsets.UTF_8).readLines()
+      var msg=""
+      for(line in lines)
+          msg=msg+line+"\n"
+      txtData.setText(msg)
+  }
+
+
+
     }
 }
