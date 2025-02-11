c8a4e27dd61af523943237984dccc4e372e8a46e
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 <frxprince@gmail.com>
2025-02-11 16:54:45 +0700

    ...

--- Android/Battery/app/src/main/java/com/example/battery/MainActivity.kt
+++ Android/Battery/app/src/main/java/com/example/battery/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.battery
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
 import android.os.Bundle
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -9,6 +14,19 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+lateinit var  receiver:Myreceiver
+abstract class Myreceiver:BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent?) {
+     val battLevel=intent?.getIntExtra(BatteryManager.EXTRA_LEVEL,0)?:0
+     val battScale=intent?.getIntExtra(BatteryManager.EXTRA_SCALE,1)?:1
+     var battvalue=(battLevel/ battScale.toDouble()) * 100
+     var usbStatus=intent?.getIntExtra(BatteryManager.EXTRA_STATUS,0)?:0
+         updateUI(battvalue.toInt(),usbStatus)
+    }
+  abstract fun updateUI(battValue:Int,usbStatus:Int)
+}
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,5 +41,26 @@ class MainActivity : AppCompatActivity() {
       progress.max=100
       progress.progress=50
 
+   receiver=object : Myreceiver() {
+            override fun updateUI(battValue: Int,usbStatus:Int) {
+              progress.progress=battValue
+    val usbMessage=when(usbStatus){
+    BatteryManager.BATTERY_STATUS_CHARGING -> "charging"
+    BatteryManager.BATTERY_STATUS_DISCHARGING ->"discharge"
+    BatteryManager.BATTERY_STATUS_FULL -> "full"
+    else-> "I don't know"
     }
+                txt.setText(usbMessage)
+            }
+
+   }
+registerReceiver(receiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+
+    }
+
+    override fun finish() {
+    unregisterReceiver(receiver)
+    }
+
+
 }
