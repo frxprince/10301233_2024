e72828b440a83220a33de7a2fab261e411914097
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 <frxprince@gmail.com>
2025-02-11 16:17:54 +0700

    ...

---
+++ Android/Battery/app/src/main/java/com/example/battery/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.battery
+
+import android.os.Bundle
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+      val txt=findViewById<TextView>(R.id.textView)
+      val progress=findViewById<ProgressBar>(R.id.progressBar)
+      progress.max=100
+      progress.progress=50
+
+    }
+}
