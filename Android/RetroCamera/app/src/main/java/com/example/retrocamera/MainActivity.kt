e56c793e8d72cc33fc27318c1051e549e284b62b
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 <frxprince@gmail.com>
2025-02-11 15:34:37 +0700

    ...

--- Android/RetroCamera/app/src/main/java/com/example/retrocamera/MainActivity.kt
+++ Android/RetroCamera/app/src/main/java/com/example/retrocamera/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.retrocamera
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -24,10 +27,18 @@ class MainActivity : AppCompatActivity() {
     val imageView=findViewById<ImageView>(R.id.imageView)
     val txt=findViewById<EditText>(R.id.editTextText)
     val btn=findViewById<Button>(R.id.button)
+    var image: Bitmap?= null
+    val rcamera=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result:ActivityResult-> if(result.resultCode== RESULT_OK){
+         if(image !=null)image?.recycle()
+        image=result.data!!.getParcelableExtra<Bitmap>("data")!!.copy(Bitmap.Config.ARGB_8888,true)
+        imageView.setImageBitmap(image)
+        }
+    }
 
         imageView.setOnClickListener {
             val i= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivity(i)
+            rcamera.launch(i)
         }
 
 
