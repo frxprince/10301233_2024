7637c3a05c44186cd5b9df180c3e9fd9a72b8d42
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2025-02-26 11:32:15 +0700

    ...

---
+++ Android/Protractor/app/src/main/java/com/example/protractor/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.protractor
+
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity(),SensorEventListener {
+    lateinit var txt1:TextView
+    lateinit var txt2:TextView
+    lateinit var seekbar:SeekBar
+    lateinit var manager:SensorManager
+    lateinit var sensor:Sensor
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    txt1=findViewById(R.id.textView);txt2=findViewById(R.id.textView2);seekbar=findViewById(R.id.seekBar)
+    manager=getSystemService(SENSOR_SERVICE) as SensorManager;sensor=manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) as Sensor
+    }
+
+    override fun onResume() {
+        manager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL)
+        super.onResume()
+    }
+
+    override fun onPause() {
+        manager.unregisterListener(this,sensor)
+        super.onPause()
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+     txt1.text="X:${p0!!.values[0]}\nY:${p0!!.values[1]}\nZ:${p0!!.values[2]}"
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int){}
+}
