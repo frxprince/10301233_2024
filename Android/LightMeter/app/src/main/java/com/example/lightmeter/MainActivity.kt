ee010a5db1b253c143d2dbb7aa99bd78a0e1aade
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2025-02-26 10:35:02 +0700

    ...

--- Android/LightMeter/app/src/main/java/com/example/lightmeter/MainActivity.kt
+++ Android/LightMeter/app/src/main/java/com/example/lightmeter/MainActivity.kt
@@ -16,6 +16,8 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
     lateinit var txt: TextView
     lateinit var lightSensor:Sensor
     lateinit var manager:SensorManager
+    lateinit var txt2:TextView
+    lateinit var proxSensor:Sensor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,14 +28,21 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
             insets
         }
      val btn_start= findViewById<Button>(R.id.button); val btn_stop=findViewById<Button>(R.id.button2)
-     txt=findViewById(R.id.textView)
+     txt=findViewById(R.id.textView); txt2=findViewById(R.id.textView2)
      manager=getSystemService(SENSOR_SERVICE) as SensorManager
      lightSensor=manager.getDefaultSensor(Sensor.TYPE_LIGHT) as Sensor
-     btn_start.setOnClickListener { manager.registerListener(this,lightSensor,SensorManager.SENSOR_DELAY_NORMAL) }
-     btn_stop.setOnClickListener { manager.unregisterListener(this,lightSensor) }
+     proxSensor=manager.getDefaultSensor(Sensor.TYPE_PROXIMITY) as Sensor
+     btn_start.setOnClickListener {
+         manager.registerListener(this,lightSensor,SensorManager.SENSOR_DELAY_NORMAL)
+         manager.registerListener(this,proxSensor,SensorManager.SENSOR_DELAY_NORMAL)
+     }
+     btn_stop.setOnClickListener {
+         manager.unregisterListener(this,lightSensor);   manager.unregisterListener(this,proxSensor)
+     }
     }
     override fun onSensorChanged(p0: SensorEvent?) {
-     txt.text=" ${p0!!.values[0]} LUX  "
+     if(p0!!.sensor == lightSensor)txt.text=" ${p0!!.values[0]} LUX  "
+     if (p0!!.sensor == proxSensor)txt2.text="${p0.values[0]} cm"
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
