e1f4e883e2453fe59d4ef688de6b21c4e6da2c42
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2025-02-26 11:49:17 +0700

    ...

--- Android/Protractor/app/src/main/java/com/example/protractor/MainActivity.kt
+++ Android/Protractor/app/src/main/java/com/example/protractor/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.PI
+import kotlin.math.atan2
 
 class MainActivity : AppCompatActivity(),SensorEventListener {
     lateinit var txt1:TextView
@@ -30,21 +32,23 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
         }
     txt1=findViewById(R.id.textView);txt2=findViewById(R.id.textView2);seekbar=findViewById(R.id.seekBar)
     manager=getSystemService(SENSOR_SERVICE) as SensorManager;sensor=manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) as Sensor
+   seekbar.max=180
+   seekbar.progressDrawable=getDrawable(R.mipmap.red)
+   seekbar.background=getDrawable(R.mipmap.white)
     }
-
     override fun onResume() {
         manager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL)
         super.onResume()
     }
-
     override fun onPause() {
         manager.unregisterListener(this,sensor)
         super.onPause()
     }
-
     override fun onSensorChanged(p0: SensorEvent?) {
      txt1.text="X:${p0!!.values[0]}\nY:${p0!!.values[1]}\nZ:${p0!!.values[2]}"
+    var degree=180.0-((atan2(p0.values[0],p0.values[1])*180.0)/ PI)-90.0
+    txt2.text="$degree "
+   seekbar.progress=  90-degree.toInt()
     }
-
     override fun onAccuracyChanged(p0: Sensor?, p1: Int){}
 }
